1f9df916f8699240d44b0b02f56551a4f72843af
Александр <alextrexen@gmail.com>
2021-08-27 15:37:53 +0300

    Исправления для декоративных элементов главной страницы

--- source/js/main-menu.js
+++ source/js/main-menu.js
@@ -1,6 +1,8 @@
 const navMain = document.querySelector('.main-nav');
 const navButton = document.querySelector('.main-nav__button-menu');
+const navList = document.querySelector('.main-nav__nav-list'); //(Скрытие меню при загрузке JS)
 
+  navList.classList.add('main-nav--disable'); //(Скрытие меню при загрузке JS)
   navMain.classList.remove('main-nav--no-js')
 
   navButton.addEventListener('click', function() {
